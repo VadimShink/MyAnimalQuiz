69cfdf4d89627916623c5faeb2c4fce2f610021e
Rigel <vadim_shink@icloud.com>
2022-07-04 21:47:51 +0300

    Add navigation controller and second screen

---
+++ MyAnimalQuiz/View Controllers/QuestionViewController.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionViewController.swift
+//  MyAnimalQuiz
+//
+//  Created by Vadim Shinkarenko on 04.07.2022.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+}
