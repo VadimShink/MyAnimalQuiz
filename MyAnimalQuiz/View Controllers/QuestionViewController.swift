66d305531a5e371cc529f2e1b5b21a79b81fdf1a
Rigel <vadim_shink@icloud.com>
2022-07-06 17:27:09 +0300

    Add functionality

--- MyAnimalQuiz/View Controllers/QuestionViewController.swift
+++ MyAnimalQuiz/View Controllers/QuestionViewController.swift
@@ -11,5 +11,39 @@ class QuestionViewController: UIViewController {
     
     @IBOutlet var myProgressView: UIProgressView!
     @IBOutlet var myQuestionLabel: UILabel!
+    @IBOutlet var myRangedSlider: UISlider!
     
+    @IBOutlet var mySingleStackView: UIStackView!
+    @IBOutlet var myMultipleStackView: UIStackView!
+    @IBOutlet var myRangedStackView: UIStackView!
+    
+    @IBOutlet var mySingleButtons: [UIButton]!
+    @IBOutlet var myMultipleLabels: [UILabel]!
+    @IBOutlet var myRangeLabels: [UILabel]!
+    
+    // MARK: - Properties
+    private let questions = Question.getQuestions()
+    private var questionIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+}
+
+// MARK: - Private
+
+extension QuestionViewController {
+    private func updateUI() {
+        // Hide stacks
+        for stackView in [mySingleStackView, myMultipleStackView, myRangedStackView] {
+            stackView?.isHidden = true
+            
+        }
+        // Get current question
+        let currentQuestion = questions[questionIndex]
+        
+        // Set current question for label
+        myQuestionLabel.text = currentQuestion.text
+    }
 }
