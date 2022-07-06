ea248486e8a664ecba9bc81bba209e1d37749af4
Rigel <vadim_shink@icloud.com>
2022-07-06 17:52:15 +0300

    Add functionality

--- MyAnimalQuiz/View Controllers/QuestionViewController.swift
+++ MyAnimalQuiz/View Controllers/QuestionViewController.swift
@@ -45,5 +45,33 @@ extension QuestionViewController {
         
         // Set current question for label
         myQuestionLabel.text = currentQuestion.text
+        
+        // Calculate progress
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        // Set progress for progressView
+        myProgressView.setProgress(totalProgress, animated: true)
+        
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        // Show current stackView
+        
+    }
+    private func showCurrentStackView(for type: ResponseType) {
+        switch type {
+        case .single:
+            break
+        case .multiple:
+            break
+        case .range:
+            break
+        }
+    }
+    private func showSingleStackView(for answers: [Answer]) {
+        mySingleStackView.isHidden = false
+        
+        for (button, answer) in zip(mySingleButtons, answers) {
+            button.setTitle(answer.text, for: .normal)
+        }
     }
 }
